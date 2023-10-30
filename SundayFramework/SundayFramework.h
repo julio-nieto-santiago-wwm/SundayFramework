f81d5afe3df34006d5a1803cb05c5100fd3e3e9c
Julio Nieto Santiago <julio.nieto@worldwidemobility.io>
2023-10-30 02:53:28 +0100

    Initial Commit

---
+++ SundayFramework/SundayFramework.h
@@ -0,0 +1,18 @@
+//
+//  SundayFramework.h
+//  SundayFramework
+//
+//  Created by Julio Nieto Santiago on 30/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SundayFramework.
+FOUNDATION_EXPORT double SundayFrameworkVersionNumber;
+
+//! Project version string for SundayFramework.
+FOUNDATION_EXPORT const unsigned char SundayFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SundayFramework/PublicHeader.h>
+
+
