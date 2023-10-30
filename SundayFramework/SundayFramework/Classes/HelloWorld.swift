37a700ea4adeeaf5d59ed015c0e05c5cbf8e4c3f
Julio Nieto Santiago <julio.nieto@worldwidemobility.io>
2023-10-30 03:31:15 +0100

    Added more files

---
+++ SundayFramework/SundayFramework/Classes/HelloWorld.swift
@@ -0,0 +1,18 @@
+//
+//  HelloWorld.swift
+//  
+//
+//  Created by Julio Nieto Santiago on 30/10/23.
+//
+
+import Foundation
+#if canImport(Alamofire)
+import Alamofire
+#endif
+
+public class HelloWorld {
+    public func printHW() {
+        
+        print("Viva el Betis")
+    }
+}
